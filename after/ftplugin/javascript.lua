0b2eb5555371c48d186e7cdb2aebfb3e8ef6b476
Tom Wieland <tom.wieland@gmail.com>
2022-12-09 23:26:49 +0100

    fix(folding): automatically fold correctly

--- after/ftplugin/javascript.lua
+++ after/ftplugin/javascript.lua
@@ -1,6 +1,6 @@
 local foldMode = require('lib.fold-mode')
 local classFound = vim.fn['search']('class ')
-if classFound then
+if classFound == 1 then
   foldMode.classFoldMode()
 else
   foldMode.functionFoldMode()
