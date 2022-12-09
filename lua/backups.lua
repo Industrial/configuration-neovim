e9f30d46baa3a276253c3d94b9c4d951b39cf5bc
Tom Wieland <tom.wieland@gmail.com>
2022-12-09 23:27:35 +0100

    fix(backups): put backups in the correct directory and add undo files

--- lua/backups.lua
+++ lua/backups.lua
@@ -1,19 +1,35 @@
-return function(use)
+local backupFiles = function(use)
   -- Make backups.
   vim.opt.backup = true
-  
+
   -- Make a backup before overwriting a file.
   vim.opt.writebackup = true
-  
-  -- TODO: Document.
-  vim.opt.swapfile = false
-  
-  -- TODO: Document.
+
+  -- Directory to keep backup files in.
+  vim.opt.backupdir = vim.fn.expand('~/.config/nvim/backup')
+
+  -- When writing a file and a backup is made, make a copy of the file and overwrite the original.
   vim.opt.backupcopy = 'yes'
-  
-  -- TODO: Document.
-  vim.opt.backupdir = 'backup'
-  
-  -- TODO: Document.
-  vim.opt.directory = 'temp'
+end
+
+local swapFiles = function(use)
+  -- Use swap files.
+  vim.opt.swapfile = true
+
+  -- Directory to put swap files in.
+  vim.opt.directory = vim.fn.expand('~/.config/nvim/temp')
+end
+
+local undoFiles = function(use)
+  -- Use undo files.
+  vim.opt.undofile = true
+
+  -- Directory to put undo files in.
+  vim.opt.undodir = vim.fn.expand('~/.config/nvim/undo')
+end
+
+return function(use)
+  backupFiles(use)
+  swapFiles(use)
+  undoFiles(use)
 end
